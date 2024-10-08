45b1b6b5c45ac187b59576f5b17d3bb0560d56f9
alabi leke <w0838407@myscc.ca>
2024-10-08 01:59:40 -0400

    completed quetion 6 and 7

--- src/app/phone-list/phone-list.component.ts
+++ src/app/phone-list/phone-list.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {phone,} from "../models/phones";
+import {Component, OnInit} from '@angular/core';
+import {phone} from "../models/phones";
 import {PhoneListItemComponent} from "../phone-list-item/phone-list-item.component";
+import {PhonesService} from "../services/phones.service";
+import * as console from "node:console";
 
 @Component({
   selector: 'app-phone-list',
@@ -11,17 +13,18 @@ import {PhoneListItemComponent} from "../phone-list-item/phone-list-item.compone
   templateUrl: './phone-list.component.html',
   styleUrl: './phone-list.component.css'
 })
-export class PhoneListComponent {
-  phoneList: phone[]=[
-    { id :1, brand: 'Apple', model: 'iPhone 16', price: 999, isSmartphone: true, },
-    { id :2, brand: 'Samsung', model: 'Galaxy S24', price: 799, isSmartphone: true },
-    { id :3, brand: 'Google', model: 'Pixel 8', price: 599, isSmartphone: true },
-    { id :4, brand: 'OnePlus', model: 'OnePlus 5', price: 729, isSmartphone: true },
-    { id :5, brand: 'Nokia', model: '3345', price: 49, isSmartphone: false },
-    { id :6, brand: 'Sony', model: 'Xperia 4', price: 899, isSmartphone: true }
+export class PhoneListComponent implements OnInit {
+  phoneList: phone[]=[];
 
-  ]
+  constructor(private phonesService: PhonesService) { }
+  ngOnInit(): void {
+    this.phonesService.getPhones().subscribe({
+      next: (phones: phone[]) => this.phoneList = phones,
+      error:(err) => console.error("error fetching phones"),
+      complete:()  => console.log("fetching completed"),
+    });
 
+  }
 
 
 }
--- src/app/services/phones.service.ts
+++ src/app/services/phones.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {phone} from "../models/phones";
+import {phoneList} from "../models/mockPhones.data";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhonesService {
+  private phone: phone[] = phoneList;
+
 
   constructor() { }
+  getPhones(): Observable<phone[]>{
+    return of(phoneList);
+  }
 }
