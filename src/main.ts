43e7d9d5f28c0ee373aeed7fcb5c171f70e3317e
alabi leke <w0838407@myscc.ca>
2024-10-09 03:46:48 -0400

    i defined the list of routes in the main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes} from "@angular/router";
+import {PhoneListComponent} from "./app/phone-list/phone-list.component";
+import {PhoneListItemComponent} from "./app/phone-list-item/phone-list-item.component";
 
-bootstrapApplication(AppComponent, appConfig)
+
+const routes:Routes = [
+  {path: 'phone', component: PhoneListComponent},
+  {path: 'phone/:id', component: PhoneListItemComponent},
+]
+bootstrapApplication(AppComponent, {
+  providers:[provideRouter(routes)
+  ]
+})
   .catch((err) => console.error(err));
